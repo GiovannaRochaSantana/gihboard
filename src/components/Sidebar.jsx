0d0ecb5ea7d284c6e523326be8d9dcf776acf7b7
giovanna rocha <giovannarochasantana4@gmail.com>
2026-08-27 18:42:40 -0300

    feat: cria componente sidebar e corrige bugs de renderizacao

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+function Sidebar() {
+    return (
+      <aside>
+        <h1>GihBoard</h1>
+  
+        <nav>
+          <p>Dashboard</p>
+          <p>Ideas</p>
+          <p>Calendar</p>
+          <p>Planning</p>
+          <p>Profile</p>
+        </nav>
+      </aside>
+    );
+  }
+  
+  export default Sidebar;
